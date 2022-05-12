87d964366fa59c7f0504d7a1237e6d556c8c46f6
warlock2409 <paradox2cr@gmail.com>
2022-05-12 10:01:17 +0530

    json linked

--- app.js
+++ app.js
@@ -1,15 +1,193 @@
 $(document).ready(function(){
-    // alert("ready");
-    
-    $("#filter").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#list #item").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-    });
-    $(".slide").click(function(){
-        console.log("clicked");
-        $(".left").toggleClass("hide");
-    })
+//  dynamic we start
+// fetch data 
+fetch('item.json')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    appendData(data);
+})
+.catch(function (err) {
+    console.log('error: ' + err);
+});
+// dynamic function
+var act=0;
+var inAct=0;
+var total=0;
+function appendData(data) {
+for (var i = 0; i < data.length; i++) {
+  total=total+1;
+if(data[i].status=="active"){
+  act=act+1;
+}else{
+  inAct=inAct+1;
+}
+}
+$(".act").text(act);
+$(".inAct").text(inAct);
+$(".total").text(total);
+}
+
+fetch("company.json")
+.then(function(read){
+  return read.json();
+})
+.then(function(info){
+  insertData(info);
+ 
+})
+.catch(function (err) {
+  console.log('error: ' + err);
+});
+
+function insertData(info){
+// compressor view
+
+var theCompressor= info.compressorView;
+var company = document.getElementById("customer")
+var td = document.createElement("td")
+td.innerText=theCompressor.company;
+td.id="name";
+company.appendChild(td);
+
+var id = document.getElementById("CustomerId")
+var td = document.createElement("td")
+td.innerText=theCompressor.id;
+td.id="name";
+id.appendChild(td);
+
+var fab = document.getElementById("FabNo")
+var td = document.createElement("td")
+td.innerText=theCompressor.fabNo;
+td.id="name";
+fab.appendChild(td);
+
+var phone = document.getElementById("phoneNo")
+var td = document.createElement("td")
+td.innerText=theCompressor.phoneNo;
+td.id="name";
+phone.appendChild(td);
+
+var update = document.getElementById("lastUpdated")
+var td = document.createElement("td")
+td.innerText=theCompressor.lastUpdate;
+td.id="name";
+update.appendChild(td);
+
+var mod= document.getElementById("mod");
+var h4 = document.createElement("h4");
+var p = document.createElement("p");
+
+p.innerText=theCompressor.company;
+h4.innerText=theCompressor.mod;
+mod.appendChild(h4);
+mod.appendChild(p);
+
+var time= document.getElementById("update");
+var span = document.createElement("span");
+span.innerText=theCompressor.lastUpdate;
+console.log(theCompressor.lastUpdate);
+time.appendChild(span)
+
+// grid view
+var grid = info.grid;
+var list = document.getElementById("gridList");
+for (let [key, value] of Object.entries(grid)) {
+  let tr = document.createElement("tr");
+  let ta = document.createElement("td");
+  let tb = document.createElement("td");
+  tb.innerText=value;
+  ta.innerText=key;
+  tr.appendChild(ta);
+  tr.appendChild(tb)
+  list.appendChild(tr);
+
+
+}
+// report
+var report = info.report[0];
+let select = document.getElementById("first");
+for(let[key,value] of Object.entries(report)){
+  let option = document.createElement("option");
+  option.setAttribute("value",key);
+  option.innerText=value;
+  select.appendChild(option);
+
+}
+var report1 = info.report[1];
+console.log(report1);
+
+let select1 = document.getElementById("rep");
+
+for(let[key,value] of Object.entries(report1)){
+  let option = document.createElement("option");
+  option.setAttribute("value",key);
+  option.innerText=value;
+  select1.appendChild(option);
+
+}
+var report2 = info.report[2];
+console.log(report2);
+
+let select2 = document.getElementById("date");
+
+for(let[key,value] of Object.entries(report2)){
+  let option = document.createElement("option");
+  option.setAttribute("value",key);
+  option.innerText=value;
+  select2.appendChild(option);
+
+}
+// trends
+var trend = info.trends[0];
+let pick = document.getElementById("compressor");
+for(let[key,value] of Object.entries(trend)){
+  let option = document.createElement("option");
+  option.setAttribute("value",key);
+  option.innerText=value;
+  pick.appendChild(option);
+
+}
+var trend1 = info.report[1];
+console.log(report1);
+
+let pick1 = document.getElementById("dates");
+
+for(let[key,value] of Object.entries(trend1)){
+  let option = document.createElement("option");
+  option.setAttribute("value",key);
+  option.innerText=value;
+  pick1.appendChild(option);
+
+}
+var trend2  = info.report[2];
+console.log(report2);
+
+let pick2 = document.getElementById("sf");
+
+for(let[key,value] of Object.entries(trend2)){
+  let option = document.createElement("option");
+  option.setAttribute("value",key);
+  option.innerText=value;
+  pick2.appendChild(option);
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    
   });
---
+++ grid.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+
+      fetch('item.json')// json file linked
+      .then(function (response) {
+       return response.json();
+   })
+   .then(function(data){
+        appendData(data);
+   })
+   .catch(function(err){
+       console.log("error:"+ err);
+   });
+
+
+   console.log(data);
+   function appendData(data){
+    var content = document.getElementById("content");
+    for(let i=0;i<data.length;i++){
+        var block = document.createElement("div");
+        block.classList.add("block")
+        block.id=data[i].id;
+        block.setAttribute("onclick","location.href='page1.html';"); // next page linked
+        var h4 = document.createElement("h4");
+        var button = document.createElement("span");
+        if(data[i].status=="active"){
+            block.classList.add("active");
+          
+          }else{
+            
+            block.classList.add("inActive");
+            
+          }
+        button.innerText=data[i].status;
+       
+
+        h4.innerText=data[i].company;
+        content.appendChild(block);
+        block.appendChild(h4);
+        block.appendChild(button);
+    }
+    // filter content
+    $("#filter").on("keyup",function(){
+      
+      var value = $(this).val().toLowerCase();
+      $("#content .block").filter(function(){
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  
+    });
+
+    
+
+    $("#icon").click(function(){
+      $(".searchInput").slideToggle();
+    });
+
+
+
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
